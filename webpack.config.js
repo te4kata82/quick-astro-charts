c5df3ccc390e90fece8c91fe26a8302b6a3465a8
Ilya Igonkin <osdev0@gmail.com>
2022-08-17 16:16:55 +0400

    Initial version

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: {
+    index: "./index.js",
+  },
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "[name].js",
+    libraryTarget: "umd",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./index.html",
+    }),
+  ],
+};
